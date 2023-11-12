5518344a5b1b06f740044e0a0d0bd9fb46d95721
Lewis Murray <lewisdmurray00@gmail.com>
2023-11-12 23:32:26 +0000

    Fixed typescript and am able to use it properly

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,11 +3,15 @@ import '../index.css'
 
 //Import components
 import Socials from './Socials';
+import MobileNav from './MobileNav';
+import Logo from '../Img/Header/Logo.png'
 
+//Import Link
+import {Link} from 'react-router-dom'
 
 const Header = () => {
   return (
-    <header className='header'>Header</header>
+    <header className='bg-pink-200 fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px]'>Header</header>
   )
 }
 
