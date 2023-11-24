4d5d24e0b975f31f6ccd436178dc6c0c8dc4ea07
Lewis Murray <lewisdmurray00@gmail.com>
2023-11-24 13:49:58 +0000

    Added the design for the header, and added the socials and mobilenav

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -13,11 +13,33 @@ const Header = () => {
   return (
     <header className='bg-pink-200 fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center '>
       
-      <div className='flex flex-col lg:flex-row lg:items-center w-full justify-between'>
-        {/* Logo */}
+      <div className='flex flex-col lg:flex-row-reverse lg:items-center w-full justify-between float-right'>
+        
+        {/* Logo
         <Link to={'/'}>
           <img src={Logo} alt='Logo' />
-        </Link>
+        </Link> */}
+
+        <nav className='hidden lg:flex gap-x-12 font-semibold '>
+          <Link to={'/'} className='text-[#696c6d] hover:text-primary transition'>
+            Home
+          </Link>
+
+          <Link to={'/about'} className='text-[#696c6d] hover:text-primary transition'>
+            About
+          </Link>
+
+          <Link to={'/portfolio'} className='text-[#696c6d] hover:text-primary transition'>
+            Portfolio
+          </Link>
+
+          <Link to={'/contact'} className='text-[#696c6d] hover:text-primary transition'>
+            Contact
+          </Link>
+        </nav>
+
+        <Socials />
+
       </div>
 
     </header>
