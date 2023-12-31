bc0b21ce3221ccb2d7854799221ba0efc399ab4c
Lewis Murray <lewisdmurray00@gmail.com>
2023-12-31 15:41:08 +0000

    FInsihed portfolio site

--- src/Pages/Portfolio.js
+++ src/Pages/Portfolio.js
@@ -36,10 +36,37 @@ const Portfolio = () => {
             <Link to={'/contact'} className='btn mb-[30px] mx-auto lg:mx-0' >Book here</Link>
           </div>
 
-          {/* Images */}
-          <div>Images</div>
-        </div>
+          {/* Image grid */}
+          <div className='grid grid-cols-2 lg:gap-2 '>
+            {/* Images */}
+            <div className='max-w-[250px] lg:max-w-[320px] h-[187px] lg:h-[220px] bg-accent overflow-hidden'>
+              <img className='object-cover h-full lg:h-[220px] hover:scale-110 transition-all duration-500 ' src={Image1} alt='image 1' />
+            </div>
 
+            <div className='max-w-[250px] lg:max-w-[320px] h-[187px] lg:h-[220px] bg-accent overflow-hidden'>
+              <img className='object-cover h-full lg:h-[220px] hover:scale-110 transition-all duration-500 ' src={Image2} alt='image 2' />
+            </div>
+
+            <div className='max-w-[250px] lg:max-w-[320px] h-[187px] lg:h-[220px] bg-accent overflow-hidden'>
+              <img className='object-cover h-full lg:h-[220px] hover:scale-110 transition-all duration-500 ' src={Image3} alt='image 3' />
+            </div>
+
+            <div className='max-w-[250px] lg:max-w-[320px] h-[187px] lg:h-[220px] bg-accent overflow-hidden'>
+              <img className='object-cover h-full lg:h-[220px] hover:scale-110 transition-all duration-500 ' src={Image4} alt='image 4' />
+            </div>
+
+            <div className='max-w-[250px] lg:max-w-[320px] h-[187px] lg:h-[220px] bg-accent overflow-hidden'>
+              <img className='object-cover h-full lg:h-[220px] hover:scale-110 transition-all duration-500 ' src={Image5} alt='image 5' />
+            </div>
+
+            <div className='max-w-[250px] lg:max-w-[320px] h-[187px] lg:h-[220px] bg-accent overflow-hidden'>
+              <img className='object-cover h-full lg:h-[220px] hover:scale-110 transition-all duration-500 ' src={Image6} alt='image 6' />
+            </div>
+
+          </div>
+          
+
+        </div>
       </div>
 
     </section>
